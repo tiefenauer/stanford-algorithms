19ed77360ebd40989f4873b282de0d31b893afb0
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-10-26 18:07:50 +0200

    add commented-out printouts for merge-sort

--- week-01/merge_sort.py
+++ week-01/merge_sort.py
@@ -1,11 +1,13 @@
 # O( n log(n) )
 def merge_sort(arr):
+    # print(arr)
     if len(arr) == 1:
         return arr
     n = len(arr)
     a = merge_sort(arr[:n // 2])
     b = merge_sort(arr[n // 2:])
 
+    # print(a + b)
     return merge(a, b, len(arr))
 
 
@@ -20,10 +22,12 @@ def merge(a, b, n):
         elif j < len(b):
             c[k] = b[j]
             j += 1
+    # print(c)
     return c
 
 
 if __name__ == '__main__':
-    arr = [5, 4, 1, 8, 7, 2, 6, 3]
+    arr = [5, 3, 8, 9, 1, 7, 0, 2, 6, 4]
+    # arr = [54, 26, 93, 17, 77, 31, 44, 55, 20]
     print(arr)
     print(merge_sort(arr))
