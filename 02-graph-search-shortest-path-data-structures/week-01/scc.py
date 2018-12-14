b60740b8875d1a6e8fa9a036d3d95ec31af71489
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-12-14 09:12:07 +0100

    performant version of Kosaraju's algorithm to compute strong components

--- 02-graph-search-shortest-path-data-structures/week-01/scc.py
+++ 02-graph-search-shortest-path-data-structures/week-01/scc.py
@@ -1,105 +1,89 @@
-class Node(object):
-
-    def __init__(self, name):
-        self.name = name
-        self.explored = False
-        self.finishing_time = None
-        self.scc_leader = False
-
-    def __str__(self):
-        return self.name
-
-    def __repr__(self):
-        return self.name
+# https://github.com/Abdallah-Elshamy/Kosaraju-Two-Pass-Algorithm/blob/master/SCC.py
 
 
-def scc(edges):
-    # reverse the graph
-    edges = reverse_edges(edges)
-
-    # initialize global variable for finishing time
-    t = 0
-
-    unexplored = find_unexplored_nodes(edges)
-    while unexplored:
-        # find highest unexplored node (by node name)
-        node = max(unexplored, key=lambda node: node.name)
-
-        # perform DFS
-        node_order = dfs(node, edges)
-
-        # set finishing times depending on node order
-        for node in node_order:
-            t += 1
-            node.finishing_time = t
-
-        unexplored = find_unexplored_nodes(edges)
-
-    # reverse graph again and reset explored flag
-    edges = reverse_edges(edges)
-    for (a, b) in edges:
-        a.explored = False
-        b.explored = False
-
-    sccs = []
-    unexplored = find_unexplored_nodes(edges)
-    while unexplored:
-        # find highest unexlored node (by finishing time)
-        node = max(unexplored, key=lambda node: node.finishing_time)
-        node_order = dfs(node, edges)
-        node_order[-1].scc_leader = True
-        sccs.append(node_order)
-        unexplored = find_unexplored_nodes(edges)
-
-    return sccs
+def dfs_1(g, i):
+    global finishing_time
+    global explored
+    global t
+    explored.add(i)
+    for j in g[i]:
+        if j not in explored:
+            dfs_1(g, j)
+    t += 1
+    finishing_time[i] = t
 
 
-def reverse_edges(edges):
-    return [(b, a) for (a, b) in edges]
+def dfs_2(g, i):
+    global sccs
+    global explored
+    global leader
+    explored.add(i)
+    sccs[i] = 1
+    for j in g[i]:
+        if j not in explored:
+            dfs_2(g, j)
+            sccs[leader] += 1
 
 
-def find_unexplored_nodes(edges):
-    return set([a for (a, b) in edges if not a.explored])
+def read_graph(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+
+    g, g_rev = {}, {}
+    for line in lines:
+        a, b = tuple(line.split())
+        a = int(a)
+        b = int(b)
+        if a not in g:
+            g[a] = []
+        if a not in g_rev:
+            g_rev[a] = []
+        if b not in g:
+            g[b] = []
+        if b not in g_rev:
+            g_rev[b] = []
+        g[a].append(b)
+        g_rev[b].append(a)
+    return g, g_rev
 
 
-def dfs(node, edges, node_order=None):
-    # mark node as visited
-    if node_order is None:
-        node_order = []
-    node.explored = True
-
-    # find unexplored neighbors (alphabetic ordering
-    neighbors = [b for (a, b) in edges if a == node and not b.explored]
-    for b in sorted(neighbors, key=lambda x: x.name):
-        dfs(b, edges, node_order)
-
-    node_order.append(node)
-    return node_order
-
-
-def main():
-    filename = 'SCC.txt'
-    # filename = 'sample_graph.txt'
-    with open(filename, 'r') as f:
-        lines = list(f.readlines())
-        edges = [(a, b) for (a, b) in [tuple(line.split()) for line in lines]]
-        nodes = {}
-        for (a, b) in edges:
-            if a not in nodes:
-                nodes[a] = Node(a)
-            if b not in nodes:
-                nodes[b] = Node(b)
-        edges = [(nodes[a], nodes[b]) for (a, b) in edges]
-
-        sccs = scc(edges)
-        sccs_by_size = sorted(sccs, key=len)
-        print(','.join(str(len(scc)) for scc in sccs_by_size[:5]))
-
-
+import resource
 import sys
-import threading
 
-sys.setrecursionlimit(800000)
-threading.stack_size(67108864)
-thread = threading.Thread(target=main)
-thread.start()
+sys.setrecursionlimit(10 ** 6)
+resource.setrlimit(resource.RLIMIT_STACK, (2 ** 29, 2 ** 30))
+
+if __name__ == '__main__':
+
+    num_nodes = 875715
+    filename = 'SCC.txt'
+    # explored nodes
+    explored = set()
+    # finishing times
+    finishing_time = {}
+    # finishing time counter
+    t = 0
+    # SCC leader
+    leader = None
+    # SCC counters
+    sccs = {}
+
+    print(f'reading {num_nodes} nodes from {filename}')
+    graph, graph_rev = read_graph(filename)
+
+    # first pass
+    print('DFS: 1st pass on reversed graph')
+    for i in range(num_nodes - 1, 0, -1):
+        if i not in explored:
+            dfs_1(graph_rev, i)
+
+    # second pass
+    print('DFS: 2nd pass on original graph')
+    explored.clear()
+    for i in sorted(list(graph.keys()), key=lambda i: finishing_time[i], reverse=True):
+        if i not in explored:
+            leader = i
+            dfs_2(graph, i)
+
+    top_scc = sorted(list(sccs.values()), reverse=True)
+    print(','.join(map(str, top_scc[:5])))
