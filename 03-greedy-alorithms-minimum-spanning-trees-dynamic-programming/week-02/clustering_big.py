0af04c6e80ec0398bac9ff62f92b210f5676bfa5
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-12-20 13:25:31 +0100

    implementation for problem 2, but with wrong results

--- 03-greedy-alorithms-minimum-spanning-trees-dynamic-programming/week-02/clustering_big.py
+++ 03-greedy-alorithms-minimum-spanning-trees-dynamic-programming/week-02/clustering_big.py
@@ -1,6 +1,5 @@
+from networkx.utils import UnionFind
 from tqdm import tqdm
-
-
 
 
 def distances_1(bit_string):
@@ -15,28 +14,45 @@ def distances_1(bit_string):
 
 
 if __name__ == '__main__':
-    filename = 'clustering_big.txt'
+    # filename = 'clustering_big.txt'
+    # filename = 'clustering_big_sample_1.txt'
+    # filename = 'input_random_30_128_24.txt' # solution: 127
+    filename = 'input_random_79_65536_24.txt'  # solution: 29407
     with open(filename, 'r') as f:
         lines = f.readlines()
-        n_nodes, n_bits = map(int, lines[0].split())
-        print(f'{n_nodes} nodes')
-        print(f'{n_bits} bits per node')
+        n_vertices, n_bits = map(int, lines[0].split())
+        print(f'{n_vertices} vertices')
+        print(f'{n_bits} bits per vertex')
 
-        distances = set()
-        zero = "0" * 24
-        distances.add(int(zero, 2))
-        d1s = distances_1(zero)
-        for d1 in d1s:
-            distances.add(int(d1, 2))
-            d2s = distances_1(d1)
-            for d2 in d2s:
-                distances.add(int(d2, 2))
+        vertices = [int(''.join(bits.split()), 2) for bits in lines[1:]]
+        print(f'{len(vertices)} unique vertices')
 
-        # nodes
-        vertices = set([int(''.join(bits.split()), 2) for bits in lines[1:]])
+        i = 0
+        d1s = set()
+        d2s = set()
+        for d1 in distances_1("0" * 24):
+            d1s.add(int(d1, 2))
+            for d2 in distances_1(d1):
+                d2s.add(int(d2, 2))
+                i += 1
+
+        print(f'{len(d1s) + len(d2s)} unique distances ({i} distances created)')
+        distances = set.union(d1s, d2s, {0})
+
         uf = UnionFind(vertices)
+        print('union-find created')
         for node in tqdm(vertices):
+            # for d1 in d1s:
+            #     neighbor = node ^ d1
+            #     if neighbor in vertices:
+            #         uf.union(node, neighbor)
+            # for d2 in d2s:
+            #     neighbor = node ^ d2
+            #     if neighbor in vertices:
+            #         uf.union(node, neighbor)
+
             for d in distances:
                 adjacent_node = node ^ d
-                if adjacent_node in vertices:
-        print(len(clusters.values()))
+                if adjacent_node in uf.parents:
+                    uf.union(node, adjacent_node)
+        print(len(list(uf.to_sets())))
